a1267cc081b5f7ad9358a6ed83f7ff325ee95da0
Kevin <murciakevin.lpz@gmail.com>
2025-12-01 00:51:17 -0500

    Se arregla error en controlador Auth

--- Proyecto-REMI-WebApi/Proyecto-REMI-WebApi/Controllers/AuthController.cs
+++ Proyecto-REMI-WebApi/Proyecto-REMI-WebApi/Controllers/AuthController.cs
@@ -14,13 +14,11 @@ namespace Proyecto_REMI_WebApi.Controllers
     public class AuthController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        private readonly IConfiguration _configuration;
         private readonly JwtHelper _jwtHelper;
 
-        public AuthController(ApplicationDbContext context, IConfiguration configuration, JwtHelper jwtHelper)
+        public AuthController(ApplicationDbContext context, JwtHelper jwtHelper)
         {
             _context = context;
-            _configuration = configuration;
             _jwtHelper = jwtHelper;
         }
 
@@ -47,7 +45,7 @@ namespace Proyecto_REMI_WebApi.Controllers
             });
         }
 
-        //REGISTER 
+        // REGISTER 
         [HttpPost("register")]
         [AllowAnonymous]
         public async Task<IActionResult> Register([FromBody] RegisterDto dto)
